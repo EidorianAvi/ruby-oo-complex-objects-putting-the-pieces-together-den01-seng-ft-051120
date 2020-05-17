a0d0f399beb5cf9ef537cc97e00c05f2c11c1936
Rene Avila <reneavila1993@gmail.com>
2020-05-17 18:15:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,5 +2,15 @@
 
 class Shoe 
   
+  attr_accessor :brand, :color, :size, :material, :condition
   
+  def initialize brand
+    @brand = brand
+  end
+
+  def cobble
+    puts "The shoe has been repaired"
+    @condition = "New"
+  
+  end
 end
